2a1ec8665a04875832aed5f75053107552b617e9
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-01-10 19:57:34 -0500

    p2 of disc2 finished

---
+++ Discussions/Discussion2/Exam_level/Book.java
@@ -0,0 +1,26 @@
+package Discussions.Discussion2.Exam_level;
+
+public class Book {
+    public String title;
+    public Library library;
+    // compiles if we add static
+
+    public static Book last = null;
+    // errors if we remove static
+
+    public Book(String name) {
+        title = name;
+        last = this;
+        library = null;
+    }
+
+    public static String lastBookTitle() {
+        return last.title;
+    }
+    // changing this method to non-static would compile
+
+    public String getTitle() {
+        return title;
+    }
+}
+
---
+++ Discussions/Discussion2/Exam_level/Library.java
@@ -0,0 +1,25 @@
+package Discussions.Discussion2.Exam_level;
+
+public class Library {
+    public Book[] books;
+    public int index;
+    public static int totalBooks = 0;
+    // Changing to non static will not error
+
+    public Library(int size) {
+        books = new Book[size];
+        index = 0;
+    }
+
+    public void addBook(Book book) {
+        books[index] = book;
+        index++;
+        totalBooks++;
+        book.library = this;
+    }
+    // changing this to static would error since books, index, total books are non static
+
+    public static void main(String[] args) {
+
+    }
+}
