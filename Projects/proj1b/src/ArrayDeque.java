9827ae80b392ced8d0e90c783a1782fa43b3b159
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-02-08 14:47:54 -0500

    Project 1b began

---
+++ Projects/proj1b/src/ArrayDeque.java
@@ -0,0 +1,55 @@
+package Projects.proj1b.src;
+
+import java.util.List;
+
+public class ArrayDeque<T> implements Deque<T> {
+
+    public ArrayDeque() {
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
+
