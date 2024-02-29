585008634442d5acb11a13732e8f95ebd8de8899
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-02-29 01:12:43 -0500

    Tweaks

--- Discussions/Discussion3/exam_level/DLList.java
+++ Discussions/Discussion3/exam_level/DLList.java
@@ -3,21 +3,6 @@ package Discussions.Discussion3.exam_level;
 import Scratchwork.SLList;
 
 public class DLList {
-    public class Node {
-        public int item;
-        public SLList.IntNode next;
 
-        public Node(int i, SLList.IntNode n) {
-            item = i;
-            next = n;
-        }
-    }
-
-    private Node first;
-    private int size;
-
-
-
-    Node sentinel;
 
 }
