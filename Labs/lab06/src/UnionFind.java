03d8eae7929025f3ceda850ff65905dabd7ead8b
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-03-13 13:52:04 -0400

    lab 6 finished

--- Labs/lab06/src/UnionFind.java
+++ Labs/lab06/src/UnionFind.java
@@ -1,3 +1,5 @@
+package Labs.lab06.src;
+
 public class UnionFind {
     /**
      * DO NOT DELETE OR MODIFY THIS, OTHERWISE THE TESTS WILL NOT PASS.
@@ -6,52 +8,88 @@ public class UnionFind {
      */
     private int[] data;
 
+
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        data = new int[N];
+        for (int i = 0; i < N; i++)
+            data[i] = -1;
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return data[find(v)] * -1;
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return data[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if (v > data.length)
+            throw new IllegalArgumentException();
+
+        int parent = parent(v);
+
+        if (parent < 0)
+            return v;
+
+        return find(parent);
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
        sets. V1 and V2 can be any element, and a union-by-size heuristic is
        used. If the sizes of the sets are equal, tie break by connecting V1's
-       root to V2's root. Union-ing a item with itself or items that are
+       root to V2's root. Union-ing an item with itself or items that are
        already connected should not change the structure. */
     public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
+        if (connected(v1, v2) || v1 == v2)
+            return;
+
+        int size1 = sizeOf(v1);
+        int size2 = sizeOf(v2);
+
+        if (size1 > size2) {
+            int newSize = sizeOf(v2);
+            data[find(v2)] = find(v1);
+            data[find(v1)] -= newSize;
+        }
+
+        if (size2 > size1) {
+            int newSize = sizeOf(v1);
+            data[find(v1)] = find(v2);
+            data[find(v1)] -= newSize;
+        }
+
+        if (size1 == size2) {
+//            int newSize = sizeOf(v2);
+//            data[find(v2)] = find(v1);
+//            data[find(v1)] -= newSize;
+            int newSize = sizeOf(v1);
+            data[find(v1)] = find(v2);
+            data[find(v1)] -= newSize;
+        }
     }
 
-    /**
-     * DO NOT DELETE OR MODIFY THIS, OTHERWISE THE TESTS WILL NOT PASS.
-     */
     public int[] returnData() {
         return data;
+    }
+
+    public static void main(String[] args) {
+        UnionFind test = new UnionFind(5);
+        test.union(0, 1);
+        test.union(2, 3);
+        test.union(0, 2);
     }
 }
--- Labs/lab06/test/UnionFindTest.java
+++ Labs/lab06/test/UnionFindTest.java
@@ -1,3 +1,6 @@
+package Labs.lab06.test;
+
+import Labs.lab06.src.UnionFind;
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -80,10 +83,34 @@ public class UnionFindTest {
         }
     }
 
-    /**
-     * Write your own tests below here to verify for correctness. The given tests are not comprehensive.
-     */
+    // own tests
+    @Test
+    public void test5 () {
+        //TODO IF YOU USE THIS TEST YOU MUST UNCOMMENT OUT THE LAST 3 LINES IN UNION
+        // this is because the lab specs and lab examples use different Union tiebreaks
 
+        int[] testData = {-4, 0, 0, 2, -1};
+        UnionFind uf = new UnionFind(5);
+        uf.union(0, 1);
+        uf.union(2, 3);
+        uf.union(0, 2);
+
+        assertThat(uf.returnData()).isEqualTo(testData);
+
+        int[] testData2 = {5, 0, 0, 2, -1, -9, 5, 5, 7, 5};
+        UnionFind uf2 = new UnionFind(10);
+        uf2.union(0, 1);
+        uf2.union(2, 3);
+        uf2.union(0, 2);
+        uf2.union(5, 6);
+        uf2.union(7, 8);
+        uf2.union(5, 8);
+        uf2.union(5, 9);
+        uf2.union(7, 2);
+
+        assertThat(uf2.returnData()).isEqualTo(testData2);
+        assertThat(uf2.connected(3, 8)).isTrue();
+    }
 }
 
 
