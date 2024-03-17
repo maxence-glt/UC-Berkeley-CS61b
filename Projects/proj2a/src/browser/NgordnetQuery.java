9b7005921a776dd10db4bc5bf0638a0ce249aa2c
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-03-16 22:37:16 -0400

    project 2a without CSV files

---
+++ Projects/proj2a/src/browser/NgordnetQuery.java
@@ -0,0 +1,12 @@
+package Projects.proj2a.src.browser;
+
+import java.util.List;
+
+/**
+ * Created by hug.
+ */
+public record NgordnetQuery(List<String> words,
+        int startYear,
+        int endYear,
+        int k) {
+}
