60b769258f2c7da0f1d873eeb1b75b82e32011c8
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-03-17 12:38:40 -0400

    lab 7 begun

---
+++ Labs/lab07/src/BSTMap.java
@@ -0,0 +1,46 @@
+package Labs.lab07.src;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V extends Comparable<V>> implements Map61B<K, V> {
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
