362dc8b1aec6cefbe5ed630d9a4d6a19458016b7
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-03-27 15:06:02 -0400

    proj2a started

---
+++ Projects/proj2b/src/main/AutograderBuddy.java
@@ -0,0 +1,14 @@
+package Projects.proj2b.src.main;
+
+import Projects.proj2b.src.browser.NgordnetQueryHandler;
+
+
+public class AutograderBuddy {
+    /** Returns a HyponymHandler */
+    public static NgordnetQueryHandler getHyponymHandler(
+            String wordFile, String countFile,
+            String synsetFile, String hyponymFile) {
+
+        throw new RuntimeException("Please fill out AutograderBuddy.java!");
+    }
+}
