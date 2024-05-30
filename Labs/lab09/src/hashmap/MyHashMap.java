8fa41c6c58f4a2aeaa3475490b19017e1b84334f
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-05-29 22:42:12 -0400

    finished hashmap

--- Labs/lab09/src/hashmap/MyHashMap.java
+++ Labs/lab09/src/hashmap/MyHashMap.java
@@ -1,8 +1,6 @@
 package Labs.lab09.src.hashmap;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation.
@@ -11,47 +9,6 @@ import java.util.Set;
  *  @author YOUR NAME HERE
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
-
-    @Override
-    public void put(K key, V value) {
-        
-    }
-
-    @Override
-    public V get(K key) {
-        return null;
-    }
-
-    @Override
-    public boolean containsKey(K key) {
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public Set<K> keySet() {
-        return null;
-    }
-
-    @Override
-    public V remove(K key) {
-        return null;
-    }
-
-    @Override
-    public Iterator<K> iterator() {
-        return null;
-    }
-
     /**
      * Protected helper class to store key/value pairs
      * The protected qualifier allows subclass access
@@ -68,12 +25,20 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     /* Instance Variables */
     private Collection<Node>[] buckets;
-    // You should probably define some more!
+    private int capacity;
+    private int intCapacity;
+    private double intLF;
+    private int size;
+    private double loadFactor;
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this(16, 0.75);
+    }
 
-    public MyHashMap(int initialCapacity) { }
+    public MyHashMap(int initialCapacity) {
+        this(initialCapacity, 0.75);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialCapacity.
@@ -82,7 +47,11 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialCapacity initial size of backing array
      * @param loadFactor maximum load factor
      */
-    public MyHashMap(int initialCapacity, double loadFactor) { }
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        intCapacity = initialCapacity;
+        intLF = loadFactor;
+        resetHelper(initialCapacity, loadFactor);
+    }
 
     /**
      * Returns a data structure to be a hash table bucket
@@ -103,10 +72,115 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
+        return new LinkedList<>();
+    }
+
+    @Override
+    public void put(K key, V value) {
+        putHelper(key, value, buckets);
+        if ((double) size / capacity > loadFactor)
+            resize();
+
+        size++;
+    }
+
+    public void resize() {
+        Collection<Node>[] tempBuckets = new Collection[capacity * 2];
+        for (Collection<Node> bucket : buckets) {
+            if (bucket == null) continue;
+            for (Node n : bucket) {
+                int position = Math.floorMod(n.key.hashCode(), tempBuckets.length);
+
+                if (tempBuckets[position] == null)
+                    tempBuckets[position] = createBucket();
+
+                tempBuckets[position].add(new Node(n.key, n.value));
+            }
+        }
+        capacity *= 2;
+        buckets = tempBuckets;
+    }
+
+    void putHelper(K key, V value, Collection[] arr) {
+        int position = Math.floorMod(key.hashCode(), arr.length);
+
+        if (containsKey(key)) {
+            ((Node) getHelper(key, true)).value = value;
+            size--;
+            return;
+        }
+
+        if (arr[position] == null)
+            arr[position] = createBucket();
+
+        arr[position].add(new Node(key, value));
+    }
+
+    @Override
+    public V get(K key) {
+        return (V) getHelper(key, null);
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return (boolean) getHelper(key, false);
+    }
+
+    private Object getHelper(K key, Object returnType) {
+        int position = Math.floorMod(key.hashCode(), capacity);
+        Collection<Node> pos = buckets[position];
+        if (pos == null) return returnType;
+        for (Node n : pos) {
+            if (n.key.hashCode() == key.hashCode()) {
+                if (returnType == null)
+                    return n.value;
+                if (returnType.equals(true))
+                    return n;
+                return true;
+            }
+        }
+
+        return returnType;
+    }
+
+    public static void main(String[] args) {
+        MyHashMap<String, Integer> m = new MyHashMap<>();
+        m.put("maxence", 18);
+        m.put("maxence", 17);
+        m.put("diane", 15);
+        m.put("raphael", 11);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        resetHelper(intCapacity, intLF);
+    }
+
+    public void resetHelper(int initialCapacity, double loadFactor) {
+        this.capacity = initialCapacity;
+        this.loadFactor = loadFactor;
+        this.size = 0;
+        buckets = new Collection[capacity];
+    }
+
+    @Override
+    public Set<K> keySet() {
         return null;
     }
 
-    // TODO: Implement the methods of the Map61B Interface below
-    // Your code won't compile until you do so!
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
 
 }
--- Scratchwork/Scratch.java
+++ Scratchwork/Scratch.java
@@ -3,10 +3,25 @@ package Scratchwork;
 import java.util.ArrayList;
 
 public class Scratch {
-    public static void main (String[] args) {
-        ArrayList<Integer> test = new ArrayList<>();
-        test.add(3);
-        test.add(4);
-        System.out.println(test);
+    static class Animal {
+        void testMethod() {
+            System.out.println("sakdjasjhd");
+        }
+        void sound() {
+            System.out.println("ygo bulls");
+        }
+    }
+
+    static class Dog extends Animal {
+        void sound() {
+            System.out.println("go umass amherst");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Animal> test = new ArrayList<>();
+        for (int i = 0; i < 1; i++)
+            test.add(new Dog());
+        test.get(0).sound();
     }
 }
